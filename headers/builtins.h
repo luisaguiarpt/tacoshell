17b5bcc4416eaeffe659936cb184490a1e72df67
pedro <pedro14carvalho@gmail.com>
2026-01-15 16:43:59 +0000

    builtin tweaks

--- headers/builtins.h
+++ headers/builtins.h
@@ -4,10 +4,13 @@
 # include "tacoshell.h"
 
 // Builtin commands
-int	ft_cd(t_core *core, char *dir_path);
-int	ft_echo(int nflag, char *arg);
-int	ft_pwd(t_core *core);
-int	ft_env(t_core *core);
+// exit -> exit_ts ([...].h)
+int		ft_cd(t_core *core, char *dir_path);
+int		ft_echo(int nflag, char *arg);
+int		ft_pwd(t_core *core);
+int		ft_env(t_core *core);
+int		ft_export(t_core *core, char *argv);
+int		ft_unset(t_core *core, char *arg);
 
 // Env
 void	env_init(t_core *core, char **envp);
@@ -15,5 +18,9 @@ void	set_env(t_core *core, char *key, char *value);
 char	*get_env(t_core *core, char *key);
 int		unset_env(t_core *core, char *key);
 void	env_split(char *env, char **key, char **value);
+
+// Utils
+void	exec_builtin(t_core *core, char *cmd, char **argv);
+int		is_builtin(char *cmd);
 
 #endif
