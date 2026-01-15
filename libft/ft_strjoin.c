cdfdfd5be9e685aefa41f0d1b2c1eee89985bc46
luis <luisaguiar@duck.com>
2026-01-15 17:59:57 +0000

    made changes to the AST file

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,50 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josepedr <josepedr@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: ldias-da <ldias-da@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/14 11:02:26 by josepedr          #+#    #+#             */
-/*   Updated: 2025/09/05 20:43:38 by josepedr         ###   ########.fr       */
+/*   Created: 2025/04/10 20:51:03 by ldias-da          #+#    #+#             */
+/*   Updated: 2025/04/15 13:38:03 by ldias-da         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
+
+/**
+ * @brief	Creates a new string and allows freeing the input
+ * @param	s1, s2	Input strings to join
+ * @param	flags	0: free s1	1: free s2	2: free both
+ * @return	Pointer to newly created joined string
+ * @notes	If no string is to be freed, use ft_strjoin
+ */
+char	*ft_strjoin2(char *s1, char *s2, int flag)
+{
+	char	*new;
+	char	*ptr;
+	char	*s1_p;
+	char	*s2_p;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s2)
+		return (s1);
+	new = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!new)
+		return (NULL);
+	ptr = new;
+	s1_p = s1;
+	s2_p = s2;
+	while (s1 && *s1)
+		*new++ = *s1++;
+	while (s2 && *s2)
+		*new++ = *s2++;
+	*new = 0;
+	if (flag == 0 || flag == 2)
+		free(s1_p);
+	if (flag == 1 || flag == 2)
+		free(s2_p);
+	return (ptr);
+}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*joined;
-	int		i;
-	int		j;
-	int		size;
+	int		len;
+	char	*new;
+	char	*ptr;
 
 	if (!s1 || !s2)
 		return (NULL);
-	i = 0;
-	j = 0;
-	size = ft_strlen(s1) + ft_strlen(s2);
-	joined = malloc((size + 1) * sizeof(char));
-	if (!joined)
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	new = malloc(sizeof(char) * len);
+	if (!new)
 		return (NULL);
-	while (s1[i] != '\0')
-	{
-		joined[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j] != '\0')
-		joined[i++] = s2[j++];
-	joined[i] = '\0';
-	return (joined);
+	ptr = new;
+	while (*s1)
+		*new++ = *s1++;
+	while (*s2)
+		*new++ = *s2++;
+	*new = 0;
+	return (ptr);
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*s1 = "Hello ";
-	char	*s2 = "world";
-	printf("%s", ft_strjoin(s1, s2));
-	return (0);
-}
-*/
--- srcs/ast.c
+++ srcs/ast.c
@@ -15,8 +15,9 @@
 bool	is_operator(t_token token);
 bool	is_operator(t_token token);
 t_token	*find_lowest_prec(t_token *start, t_token *end);
+void	*free_mem_arr(char **arr, int index);
 
-t_ast	create_ast(t_core)
+t_ast	create_ast(t_core *core)
 {
 	t_token	*start;
 	t_token	*end;
@@ -26,26 +27,26 @@ t_ast	create_ast(t_core)
 	end = start;
 	while (end->next)
 		end = end->next;
-	ast = parse_tokens(start, end);
+	ast = parse_tokens(start, end, core);
 	return (ast);
 }
 
 // Need a function to create a node out of an operator
 // and then a function to create a node out of a chain of tokens (commands)
-t_ast	*parse_tokens(t_token *start, t_token *end)
+t_ast	*parse_tokens(t_token *start, t_token *end, t_core *core)
 {
 	t_token	*op;
 	t_ast	node;
 
 	op = find_lowest_prec(start, end);
 	if (!op)
-		return (create_node(CMD, start, end));
+		return (create_node(CMD, start, end, core));
 	else
-		return (create_node(PIPE, start, end));
+		return (create_node(PIPE, start, end, core));
 	node = create_node(op);
 }
 
-t_ast	*create_node(t_token_type type, t_token *start, t_token *end)
+t_ast	*create_node(t_token_type type, t_token *start, t_token *end, t_core *core)
 {
 	t_ast	*node;
 
@@ -53,8 +54,100 @@ t_ast	*create_node(t_token_type type, t_token *start, t_token *end)
 	node->type = type;
 	if (type == CMD)
 	{
-		node->args = gen_args(start, end);
+		node->cmd = gen_cmd(start, end, core);
+    } 
 	return (node);
+}
+
+t_ast_cmd   gen_cmd(t_token *start, t_token *end, t_core *core)
+{
+	t_ast_cmd	cmd;
+
+	cmd = wr_calloc(1, sizeof(t_ast_cmd), core);
+	cmd.argv = gen_argv(start, end, core);
+	cmd.cmd_path = get_path(argv[0], core.env);
+}
+
+char	*get_path(char *av_cmd, char **ep)
+{
+	int		i;
+	char	**cmd;
+	char	**paths;
+	char	*cmd_path;
+
+	cmd = ft_split(av_cmd, ' ');
+	paths = ft_split(get_env(core, "PATH"), ':');
+	i = -1;
+	while (paths[++i])
+	{
+		cmd_path = ft_strjoin2(ft_strjoin(paths[i], "/"), cmd[0], 0);
+		if (access(cmd_path, X_OK) == 0)
+		{
+			ft_free_tab(cmd);
+			ft_free_tab(paths);
+			return (cmd_path);
+		}
+		free(cmd_path);
+	}
+	ft_free_tab(cmd);
+	ft_free_tab(paths);
+	return (NULL);
+}
+
+int		count_tokens(t_token *start, t_token *end)
+{
+	int		n;
+	t_token	*curr;
+
+	n = 0;
+	curr = start;
+	while (curr && curr != end)
+	{
+		n++;
+		curr = curr->next;
+	}
+	if (curr == end)
+		n++;
+	return (n);
+}
+
+char	**gen_argv(t_token *start, t_token *end, t_core *core)
+{
+	t_token	*token;
+	char	**argv;
+	int		n_tokens;
+	int		i;
+
+
+	token = start;
+	n_tokens = count_tokens(start, end);
+	argv = wr_calloc(n_tokens + 1, sizeof(char *), core);
+	i = 0;
+	while (i < n_tokens)
+	{
+		argv[i] = ft_substr(core.line, token->start, token->length);
+		if (!argv[i])
+			return (free_mem_arr(argv, i));
+		i++;
+	}
+	argv[i] = NULL;
+	return (argv);
+}
+
+void	*free_mem_arr(char **arr, int index)
+{
+	int i;
+
+	i = 0;
+	if (!arr)
+		return (NULL);
+	while (i < index)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+	return (NULL);
 }
 
 t_token	*find_lowest_prec(t_token *start, t_token *end)
