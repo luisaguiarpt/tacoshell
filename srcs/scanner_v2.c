9923d8b9467214cfb09d3ed3c109f393c92e7232
luis <luisaguiar@duck.com>
2026-01-30 01:02:14 +0000

    changed scanner to behave like shell. quotes no longer delimitate token

---
+++ srcs/scanner_v2.c
@@ -0,0 +1,57 @@
+#include "../headers/tacoshell.h"
+
+t_token	scan_token(t_scanner *scanner)
+{
+	char		c;
+
+	skip_space(scanner);
+	scanner->start = scanner->current;
+	if (is_at_end(scanner))
+		return (create_token(EOF_TOK, scanner));
+	c = advance(scanner);
+	if (c == '|')
+		return (create_token(PIPE, scanner));
+	if (c == '<')
+	{
+		if (match('<', scanner))
+			return (create_token(HERE_DOC, scanner));
+		else
+			return (create_token(REDIR_IN, scanner));
+	}
+	if (c == '>')
+	{
+		if (match('>', scanner))
+			return (create_token(APPEND, scanner));
+		else
+			return (create_token(REDIR_OUT, scanner));
+	}
+	if (c == '(' || c == ')')
+		return (error_token("Syntax error near ( or )."));
+	if (c == ';' || c == '&')
+		return (create_token(EOF_TOK, scanner));
+	return (scan_word(scanner, c));
+}
+// Peek returns the current character
+// Advance returns the current character and advances to the next character
+// Match checks to see if the argument matches the current character, if so, advances to the next character
+// is_at_end returns true if current character is \0
+
+t_token scan_word(t_scanner *scanner, char c)
+{
+	while (!(is_metachar(peek(scanner)) && scanner->state == NEUTRAL) && !is_at_end(scanner))
+	{
+		c = peek(scanner);
+		if (c == '\'' && scanner->state == NEUTRAL)
+			scanner->state = IN_SINGLE_QUOTES;
+		else if (c == '\'' && scanner->state == IN_SINGLE_QUOTES)
+			scanner->state = NEUTRAL;
+		if (c == '"' && scanner->state == NEUTRAL)
+			scanner->state = IN_DOUBLE_QUOTES;
+		else if (c == '"' && scanner->state ==  IN_DOUBLE_QUOTES)
+			scanner->state = NEUTRAL;
+		c = advance(scanner);
+		if (is_at_end(scanner) && scanner->state != NEUTRAL)
+			return(error_token("Unterminated string."));
+	}
+	return (create_token(WORD, scanner));
+}
