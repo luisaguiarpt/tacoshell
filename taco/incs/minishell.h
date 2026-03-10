ee978bb957bcd705b43d76fde6e1e184ce13aa31
luis <ldias-da@student.42porto.com>
2026-03-10 16:21:32 +0000

    Restructuring: ongoing

--- taco/incs/minishell.h
+++ taco/incs/minishell.h
@@ -30,6 +30,7 @@
 # include <unistd.h>
 
 // Includes
+# include "typedefs.h"
 # include "ast.h"
 # include "builtins.h"
 # include "shell.h"
@@ -46,18 +47,6 @@
 # include <libft.h>
 
 extern int	g_signal;
-
-// Typedefs
-// Structs
-typedef struct s_shell		t_shell;
-typedef struct s_variable	t_variable;
-typedef struct s_lexer		t_lexer;
-typedef struct s_token		t_token;
-typedef struct s_token_list	t_token_list;
-
-// enum
-typedef enum e_token_type	t_token_type;
-typedef enum e_state		t_state;
 
 // main.c - Main functions
 void	eval_loop(t_shell *shell);
---
+++ taco/incs/typedefs.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   typedefs.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josepedr <josepedr@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 15:23:19 by josepedr          #+#    #+#             */
+/*   Updated: 2026/03/10 15:23:20 by josepedr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TYPEDEFS_H
+# define TYPEDEFS_H
+
+// Structs
+typedef struct s_shell		t_shell;
+typedef struct s_variable	t_variable;
+typedef struct s_lexer		t_lexer;
+typedef struct s_token		t_token;
+typedef struct s_token_list	t_token_list;
+
+// enum
+typedef enum e_token_type	t_token_type;
+typedef enum e_state		t_state;
+
+#endif
